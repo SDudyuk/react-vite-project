a6a18ae5e8157e6e88d50e868ef24c2aa35355aa
SDudyuk <s.dudyuk.o@gmail.com>
2026-01-02 16:28:40 +0200

    create first component

---
+++ src/components/HillelCourses.jsx
@@ -0,0 +1,14 @@
+const HillelCourses = () => {
+  return (
+    <aside>
+      <h2>Hillel courses:</h2>
+      <ul>
+        <li>JavaScript Basic</li>
+        <li>JavaScript Advanced</li>
+        <li>React</li>
+      </ul>
+    </aside>
+  )
+}
+
+export default HillelCourses;
